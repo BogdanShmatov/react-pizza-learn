402742ae2441680e3e40b5a4c7002829f694c264
Bogdan <shmatovb@mail.ru>
2020-08-27 22:35:52 +0600

    React-router-dom added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router} from 'react-router-dom';
+
 
 import './scss/app.scss';
 
@@ -8,7 +10,11 @@ import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App/>
+       {/* <Route exact path='/' component={App} /> */}
+       {/* <Route exact path='/head' component={Header} /> */}
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
